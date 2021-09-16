bfb846c17905e691a4efb03aaaf3de25a92a75d6
Ahmed Hamed <ahmedhrayyan@outlook.com>
2021-09-16 21:16:06 +0200

    Add Contact model
    Add Contact Model and all of its related models like Type & Phone

--- db/models.py
+++ db/models.py
@@ -1,6 +1,7 @@
 import bcrypt
 from datetime import datetime
 from sqlalchemy import exc, Column, Integer, VARCHAR, Text, DateTime, LargeBinary
+from sqlalchemy.sql.schema import ForeignKey
 from db import db
 
 
@@ -44,6 +45,8 @@ class User(db.Model, BaseModel):
     password = Column(LargeBinary, nullable=False)
     avatar = Column(Text, nullable=True)
     created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    contacts = db.relationship(
+        'Contact', order_by='desc(Contact.id)', lazy=True, cascade='all')
 
     def __init__(self, name, email, password):
         self.name = name
@@ -62,3 +65,43 @@ class User(db.Model, BaseModel):
         '''
         self.password = bcrypt.hashpw(
             bytes(password, 'utf-8'), bcrypt.gensalt(12))
+
+
+class Contact(db.Model, BaseModel):
+    __tablename__ = "contacts"
+    id = Column(Integer, primary_key=True)
+    name = Column(VARCHAR, nullable=False)
+    email = Column(VARCHAR, nullable=True)
+    avatar = Column(Text, nullable=True)
+    phones = db.relationship(
+        'Phone', order_by='desc(Phone.id)', lazy=True, cascade='all')
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+
+    def __init__(self, user_id: int, name: str, email: str = None, avatar: str = None):
+        self.user_id = user_id
+        self.name = name
+        self.email = email
+        self.avatar = avatar
+
+class Type(db.Model, BaseModel):
+    __tablename__ = "types"
+    id = Column(Integer, primary_key=True)
+    value = Column(VARCHAR, nullable=False)
+
+    def __init__(self, value: str):
+        self.value = value
+
+
+class Phone(db.Model, BaseModel):
+    __tablename__ = "phones"
+    id = Column(Integer, primary_key=True)
+    value = Column(VARCHAR, nullable=False)
+    type_id = Column(Integer, ForeignKey('types.id'), nullable=False)
+    type = db.relationship('Type')
+    contact_id = Column(Integer, ForeignKey('contacts.id'), nullable=False)
+
+    def __init__(self, value: str, type_id: int, contact_id: int):
+        self.value = value
+        self.type_id = type_id
+        self.contact_id = contact_id
